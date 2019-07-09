93841bcf16b6af2f5dd4916dfd2afaa0dfa183e1
wanghao07456 <wanghao6677@gmail.com>
2019-07-09 10:36:57 +0800

    Add files via upload

---
+++ 数组/day2/二维数组中的查找/Search_a_2D_Matrix_taiyang.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0){
+            return false;
+        }
+        
+        if(matrix[0] == null || matrix[0].length == 0){
+            return false;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = 0;
+        int j = n - 1;
+        while (i < m && j >= 0) {
+            //i,j的定义已经在右上角了
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
