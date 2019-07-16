cd3778c9b2ba954566aff8b305d6ee8a28d26ef6
wanghao07456 <wanghao6677@gmail.com>
2019-07-16 10:43:48 +0800

    Add files via upload

---
+++ 链表/删除链表中的元素/removeElements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        
+        while (head.next != null) {
+            if (head.next.val == val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;//move head pointer to next and then loop
+            }
+        }
+        return dummy.next;
+    }
+}
